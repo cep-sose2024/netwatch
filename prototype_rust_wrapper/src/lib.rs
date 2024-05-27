a259f287db5d4918420bf2128fc5996edb3ac556
Mark Beck <mark-beck@web.de>
2024-05-27 12:39:13 +0200

    feat: changed rust_wrapper to use the forked repo

--- prototype_rust_wrapper/src/lib.rs
+++ prototype_rust_wrapper/src/lib.rs
@@ -8,24 +8,44 @@ use crypto_layer::{
             KeyUsage,
         },
         error::SecurityModuleError,
-        factory::{SecModules, SecurityModule},
+        factory::{SecModules, SecurityModule}
     },
-    tpm::core::instance::TpmType,
+    tpm::{android::config::AndroidConfig, core::instance::TpmType},
 };
-use jni::{
-    objects::{JByteArray, JClass, JString},
+use robusta_jni::jni::{
+    objects::{JClass, JString, JObject},
     sys::{jboolean, jbyteArray},
-    JNIEnv,
+    JNIEnv, JavaVM,
 };
 use tracing::{debug, error, warn};
 
-fn generate_new_key(key: String, algorithm: String) -> Result<(), SecurityModuleError> {
+fn generate_new_key(key: String, algorithm: String, vm: JavaVM) -> Result<(), SecurityModuleError> {
+
+    let algorithm = match algorithm.borrow() {
+        "RSA" => AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512),
+        "EC" => AsymmetricEncryption::Ecc(algorithms::encryption::EccSchemeAlgorithm::Null),
+        _ => AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512),
+    };
+
+    let config = AndroidConfig {
+        key_algo: Some(algorithm),
+        sym_algo: None,
+        hash: None,
+        key_usages: Some(vec![
+            KeyUsage::Decrypt,
+            KeyUsage::SignEncrypt,
+            KeyUsage::CreateX509,
+        ]),
+        vm: Some(vm),
+    };
+
     debug!("generating key: {key}");
     let provider = SecModules::get_instance(
         key.clone(),
         SecurityModule::Tpm(TpmType::Android(
             crypto_layer::tpm::core::instance::AndroidTpmType::Keystore,
         )),
+        None,
     )
     .ok_or(SecurityModuleError::InitializationError(
         "couldn't create key provider".to_owned(),
@@ -33,19 +53,8 @@ fn generate_new_key(key: String, algorithm: String) -> Result<(), SecurityModule
 
     let mut provider = provider.lock().unwrap();
 
-    let key_usage = vec![
-        KeyUsage::Decrypt,
-        KeyUsage::SignEncrypt,
-        KeyUsage::CreateX509,
-    ];
-    let algorithm = match algorithm.borrow() {
-        "RSA" => AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512),
-        "EC" => AsymmetricEncryption::Ecc(algorithms::encryption::EccSchemeAlgorithm::Null),
-        _ => AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512),
-    };
-
-    provider.initialize_module(algorithm, None, None, key_usage)?;
-    provider.create_key(&key)?;
+    provider.initialize_module()?;
+    provider.create_key(&key, Box::new(config))?;
     Ok(())
 }
 
@@ -56,23 +65,26 @@ pub unsafe extern "C" fn Java_com_netwatch_RustNetWatch_generateNewKey(
     key_id: JString,
     algorithm: JString,
 ) {
-    let key_id: String = env.get_string(&key_id).expect("Couldn't get key ID").into();
+    let key_id: String = env.get_string(key_id).expect("Couldn't get key ID").into();
     let algorithm: String = env
-        .get_string(&algorithm)
+        .get_string(algorithm)
         .expect("Couldn't get algo")
         .into();
 
-    if let Err(e) = generate_new_key(key_id, algorithm) {
+    let vm = env.get_java_vm().unwrap();
+
+    if let Err(e) = generate_new_key(key_id, algorithm, vm) {
         handle_error(&mut env, e);
     }
 }
 
-fn encrypt(key: String, bytes: &[u8]) -> Result<Vec<u8>, SecurityModuleError> {
+fn encrypt(key: String, bytes: &[u8], vm: JavaVM) -> Result<Vec<u8>, SecurityModuleError> {
     let provider = SecModules::get_instance(
         key.clone(),
         SecurityModule::Tpm(TpmType::Android(
             crypto_layer::tpm::core::instance::AndroidTpmType::Keystore,
         )),
+        None
     )
     .ok_or(SecurityModuleError::InitializationError(
         "couldn't create key provider".to_owned(),
@@ -80,20 +92,21 @@ fn encrypt(key: String, bytes: &[u8]) -> Result<Vec<u8>, SecurityModuleError> {
 
     let mut provider = provider.lock().unwrap();
 
-    let key_usage = vec![
-        KeyUsage::Decrypt,
-        KeyUsage::SignEncrypt,
-        KeyUsage::CreateX509,
-    ];
+    let config = AndroidConfig {
+        key_algo: Some(AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512)),
+        sym_algo: None,
+        hash: None,
+        key_usages: Some(vec![
+            KeyUsage::Decrypt,
+            KeyUsage::SignEncrypt,
+            KeyUsage::CreateX509,
+        ]),
+        vm: Some(vm),
+    };
     provider
-        .initialize_module(
-            AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512),
-            None,
-            None,
-            key_usage,
-        )
+        .initialize_module()
         .unwrap();
-    provider.load_key(&key).unwrap();
+    provider.load_key(&key, Box::new(config)).unwrap();
 
     let bytes = provider.encrypt_data(bytes)?;
 
@@ -105,19 +118,21 @@ pub unsafe extern "C" fn Java_com_netwatch_RustNetwatch_encrypt(
     mut env: JNIEnv,
     _: JClass,
     key_id: JString,
-    array_ref: JByteArray,
+    array_ref: jbyteArray,
 ) -> jbyteArray {
-    let key_id: String = env.get_string(&key_id).expect("Couldn't get key ID").into();
+    let key_id: String = env.get_string(key_id).expect("Couldn't get key ID").into();
 
-    let length = env.get_array_length(&array_ref).unwrap();
+    let length = env.get_array_length(array_ref).unwrap();
     let mut bytes = vec![0; length as usize];
-    env.get_byte_array_region(&array_ref, 0, &mut bytes)
+    env.get_byte_array_region(array_ref, 0, &mut bytes)
         .unwrap();
 
     // the bytes are i8 right now, we need to reinterpret them to u8
     let bytes = bytemuck::cast_slice::<i8, u8>(bytes.as_slice());
 
-    match encrypt(key_id, bytes) {
+    let vm = env.get_java_vm().unwrap();
+
+    match encrypt(key_id, bytes, vm) {
         Ok(bytes) => {
             // now we need to turn them back into i8
             let bytes = bytemuck::cast_slice::<u8, i8>(bytes.as_slice());
@@ -125,22 +140,23 @@ pub unsafe extern "C" fn Java_com_netwatch_RustNetwatch_encrypt(
             let output = env
                 .new_byte_array(bytes.len() as i32)
                 .expect("Couldn't create java array");
-            env.set_byte_array_region(&output, 0, bytes).unwrap();
-            **output
+            env.set_byte_array_region(output, 0, bytes).unwrap();
+            output
         }
         Err(e) => {
             handle_error(&mut env, e);
-            jni::objects::JObject::null().as_raw()
+            *JObject::null()
         }
     }
 }
 
-fn decrypt(key_id: String, bytes: &[u8]) -> Result<Vec<u8>, SecurityModuleError> {
+fn decrypt(key_id: String, bytes: &[u8], vm: JavaVM) -> Result<Vec<u8>, SecurityModuleError> {
     let provider = SecModules::get_instance(
         key_id.clone(),
         SecurityModule::Tpm(TpmType::Android(
             crypto_layer::tpm::core::instance::AndroidTpmType::Keystore,
         )),
+        None
     )
     .ok_or(SecurityModuleError::InitializationError(
         "couldn't create key provider".to_owned(),
@@ -148,20 +164,22 @@ fn decrypt(key_id: String, bytes: &[u8]) -> Result<Vec<u8>, SecurityModuleError>
 
     let mut provider = provider.lock().unwrap();
 
-    let key_usage = vec![
-        KeyUsage::Decrypt,
-        KeyUsage::SignEncrypt,
-        KeyUsage::CreateX509,
-    ];
+    let config = AndroidConfig {
+        key_algo: Some(AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512)),
+        sym_algo: None,
+        hash: None,
+        key_usages: Some(vec![
+            KeyUsage::Decrypt,
+            KeyUsage::SignEncrypt,
+            KeyUsage::CreateX509,
+        ]),
+        vm: Some(vm),
+    };
+
     provider
-        .initialize_module(
-            AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512),
-            None,
-            None,
-            key_usage,
-        )
+        .initialize_module()
         .unwrap();
-    provider.load_key(&key_id).unwrap();
+    provider.load_key(&key_id, Box::new(config)).unwrap();
 
     let bytes = provider.decrypt_data(bytes)?;
     return Ok(bytes);
@@ -172,19 +190,21 @@ pub unsafe extern "C" fn Java_com_netwatch_RustNetWatch_decrypt(
     mut env: JNIEnv,
     _: JClass,
     key_id: JString,
-    array_ref: JByteArray,
+    array_ref: jbyteArray,
 ) -> jbyteArray {
-    let key_id: String = env.get_string(&key_id).expect("Couldn't get key ID").into();
+    let key_id: String = env.get_string(key_id).expect("Couldn't get key ID").into();
 
-    let length = env.get_array_length(&array_ref).unwrap();
+    let length = env.get_array_length(array_ref).unwrap();
     let mut bytes = vec![0; length as usize];
-    env.get_byte_array_region(&array_ref, 0, &mut bytes)
+    env.get_byte_array_region(array_ref, 0, &mut bytes)
         .unwrap();
 
     // the bytes are i8 right now, we need to reinterpret them to u8
     let bytes = bytemuck::cast_slice::<i8, u8>(bytes.as_slice());
 
-    match decrypt(key_id, bytes) {
+    let vm = env.get_java_vm().unwrap();
+
+    match decrypt(key_id, bytes, vm) {
         Ok(bytes) => {
             // now we need to turn them back into i8
             let bytes = bytemuck::cast_slice::<u8, i8>(bytes.as_slice());
@@ -192,22 +212,23 @@ pub unsafe extern "C" fn Java_com_netwatch_RustNetWatch_decrypt(
             let output = env
                 .new_byte_array(bytes.len() as i32)
                 .expect("Couldn't create java array");
-            env.set_byte_array_region(&output, 0, bytes).unwrap();
-            **output
+            env.set_byte_array_region(output, 0, bytes).unwrap();
+            output
         }
         Err(e) => {
             handle_error(&mut env, e);
-            jni::objects::JObject::null().as_raw()
+            *JObject::null()
         }
     }
 }
 
-fn sign(key_id: String, bytes: &[u8]) -> Result<Vec<u8>, SecurityModuleError> {
+fn sign(key_id: String, bytes: &[u8], vm: JavaVM) -> Result<Vec<u8>, SecurityModuleError> {
     let provider = SecModules::get_instance(
         key_id.clone(),
         SecurityModule::Tpm(TpmType::Android(
             crypto_layer::tpm::core::instance::AndroidTpmType::Keystore,
         )),
+        None
     )
     .ok_or(SecurityModuleError::InitializationError(
         "couldn't create key provider".to_owned(),
@@ -215,20 +236,24 @@ fn sign(key_id: String, bytes: &[u8]) -> Result<Vec<u8>, SecurityModuleError> {
 
     let mut provider = provider.lock().unwrap();
 
-    let key_usage = vec![
-        KeyUsage::Decrypt,
-        KeyUsage::SignEncrypt,
-        KeyUsage::CreateX509,
-    ];
+    let config = AndroidConfig {
+        key_algo: Some(AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512)),
+        sym_algo: None,
+        hash: None,
+        key_usages: Some(vec![
+            KeyUsage::Decrypt,
+            KeyUsage::SignEncrypt,
+            KeyUsage::CreateX509,
+        ]),
+        vm: Some(vm),
+    };
+
     provider
-        .initialize_module(
-            AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512),
-            None,
-            None,
-            key_usage,
-        )
+        .initialize_module()
         .unwrap();
-    provider.load_key(&key_id).unwrap();
+
+
+    provider.load_key(&key_id, Box::new(config)).unwrap();
 
     let bytes = provider.sign_data(bytes)?;
     return Ok(bytes);
@@ -239,19 +264,21 @@ pub unsafe extern "C" fn Java_com_netwatch_RustNetWatch_sign(
     mut env: JNIEnv,
     _: JClass,
     key_id: JString,
-    array_ref: JByteArray,
+    array_ref: jbyteArray,
 ) -> jbyteArray {
-    let key_id: String = env.get_string(&key_id).expect("Couldn't get key ID").into();
+    let key_id: String = env.get_string(key_id).expect("Couldn't get key ID").into();
 
-    let length = env.get_array_length(&array_ref).unwrap();
+    let length = env.get_array_length(array_ref).unwrap();
     let mut bytes = vec![0; length as usize];
-    env.get_byte_array_region(&array_ref, 0, &mut bytes)
+    env.get_byte_array_region(array_ref, 0, &mut bytes)
         .unwrap();
 
     // the bytes are i8 right now, we need to reinterpret them to u8
     let bytes = bytemuck::cast_slice::<i8, u8>(bytes.as_slice());
 
-    match sign(key_id, bytes) {
+    let vm = env.get_java_vm().unwrap();
+
+    match sign(key_id, bytes, vm) {
         Ok(bytes) => {
             // now we need to turn them back into i8
             let bytes = bytemuck::cast_slice::<u8, i8>(bytes.as_slice());
@@ -259,12 +286,12 @@ pub unsafe extern "C" fn Java_com_netwatch_RustNetWatch_sign(
             let output = env
                 .new_byte_array(bytes.len() as i32)
                 .expect("Couldn't create java array");
-            env.set_byte_array_region(&output, 0, bytes).unwrap();
-            **output
+            env.set_byte_array_region(output, 0, bytes).unwrap();
+            output
         }
         Err(e) => {
             handle_error(&mut env, e);
-            jni::objects::JObject::null().as_raw()
+            *JObject::null()
         }
     }
 }
@@ -273,12 +300,14 @@ fn verify(
     key_id: String,
     data_bytes: &[u8],
     signature_bytes: &[u8],
+    vm: JavaVM
 ) -> Result<bool, SecurityModuleError> {
     let provider = SecModules::get_instance(
         key_id.clone(),
         SecurityModule::Tpm(TpmType::Android(
             crypto_layer::tpm::core::instance::AndroidTpmType::Keystore,
         )),
+        None,
     )
     .ok_or(SecurityModuleError::InitializationError(
         "couldn't create key provider".to_owned(),
@@ -286,20 +315,22 @@ fn verify(
 
     let mut provider = provider.lock().unwrap();
 
-    let key_usage = vec![
-        KeyUsage::Decrypt,
-        KeyUsage::SignEncrypt,
-        KeyUsage::CreateX509,
-    ];
+    let config = AndroidConfig {
+        key_algo: Some(AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512)),
+        sym_algo: None,
+        hash: None,
+        key_usages: Some(vec![
+            KeyUsage::Decrypt,
+            KeyUsage::SignEncrypt,
+            KeyUsage::CreateX509,
+        ]),
+        vm: Some(vm),
+    };
+
     provider
-        .initialize_module(
-            AsymmetricEncryption::Rsa(algorithms::KeyBits::Bits512),
-            None,
-            None,
-            key_usage,
-        )
+        .initialize_module()
         .unwrap();
-    provider.load_key(&key_id).unwrap();
+    provider.load_key(&key_id, Box::new(config)).unwrap();
 
     provider.verify_signature(data_bytes, signature_bytes)
 }
@@ -309,26 +340,28 @@ pub unsafe extern "C" fn Java_com_netwatch_RustNetWatch_verify(
     mut env: JNIEnv,
     _: JClass,
     key_id: JString,
-    data_ref: JByteArray,
-    signature_ref: JByteArray,
+    data_ref: jbyteArray,
+    signature_ref: jbyteArray,
 ) -> jboolean {
-    let key_id: String = env.get_string(&key_id).expect("Couldn't get key ID").into();
+    let key_id: String = env.get_string(key_id).expect("Couldn't get key ID").into();
 
-    let data_length = env.get_array_length(&data_ref).unwrap();
+    let data_length = env.get_array_length(data_ref).unwrap();
     let mut data_bytes = vec![0; data_length as usize];
-    env.get_byte_array_region(&data_ref, 0, &mut data_bytes)
+    env.get_byte_array_region(data_ref, 0, &mut data_bytes)
         .unwrap();
 
-    let signature_length = env.get_array_length(&signature_ref).unwrap();
+    let signature_length = env.get_array_length(signature_ref).unwrap();
     let mut signature_bytes = vec![0; signature_length as usize];
-    env.get_byte_array_region(&signature_ref, 0, &mut signature_bytes)
+    env.get_byte_array_region(signature_ref, 0, &mut signature_bytes)
         .unwrap();
 
     // the bytes are i8 right now, we need to reinterpret them to u8
     let data_bytes = bytemuck::cast_slice::<i8, u8>(data_bytes.as_slice());
     let signature_bytes = bytemuck::cast_slice::<i8, u8>(signature_bytes.as_slice());
 
-    match verify(key_id, data_bytes, signature_bytes) {
+    let vm = env.get_java_vm().unwrap();
+
+    match verify(key_id, data_bytes, signature_bytes, vm) {
         Ok(true) => 1,
         Ok(false) => 0,
         Err(e) => {
